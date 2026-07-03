b925f4735353620c04f8eda14d157c228b411fd2
MarianaGonzalez28 <marixd2800@gmail.com>
2026-07-03 10:22:48 -0600

    Agregando proyecto prueba

---
+++ prueba/registros/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+class Alumnos(models.Model): #Define la escructura de nuestra tabla
+    matricula = models.CharField(max_length=12, verbose_name="Clave") #Texto corto
+    nombre = models.CharField() #Texto largo
+    carrera = models.CharField()
+    turno = models.CharField(max_length=10)
+    imagen = models.ImageField(null=True, upload_to="fotos", verbose_name="Fotografía") 
+    created = models.DateTimeField(auto_now_add=True) #Fecha y tiempo
+    updated = models.DateTimeField(auto_now_add=True) 
+
+    class Meta:
+        verbose_name = "Alumno"
+        verbose_name_plural = "Alumnos"
+        ordering = ["-created"]
+        #el menos indica que se ordenara del más reciente al más viejo
+
+    def __str__(self):
+        return self.nombre
+        #Indica que se mostrara el nombre como valor de la tabla
+
+class Comentario(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="Clave")
+    alumno = models.ForeignKey(Alumnos, on_delete=models.CASCADE, verbose_name="Alumno")
+    #on_delete=models.CASCADE, verbose_name="Alumno" #indica que si se elimina un alumno, se eliminaran todos sus comentarios
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Registrado")
+    coment = RichTextField(verbose_name="Comentario")
+
+    class Meta:
+        verbose_name = "Comentario"
+        verbose_name_plural = "Comentarios"
+        ordering = ["-created"]
+    
+    def __str__(self):
+        return self.coment
+
+class ComentarioContacto(models.Model):
+    id = models.AutoField(primary_key=True,verbose_name="Clave")
+    usuario = models.TextField(verbose_name="Usuario")
+    mensaje = models.TextField(verbose_name="Comentario") 
+    created =models.DateTimeField(auto_now_add=True,verbose_name="Registrado") 
+
+    class Meta:
+        verbose_name = "Comentario Contacto"
+        verbose_name_plural = "Comentarios Contactos"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.mensaje 
+        #Indica que se mostrára el mensaje como valor en la tabla
---
+++ prueba/registros/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import Alumnos
+# Accedemos al modelo Alumnos que contine la esctructura de la tabla
+from .forms import ComentarioContactoForm
+
+# Create your views here.
+def registros(request):
+    alumnos = Alumnos.objects.all()
+    # all recuper todos los objetos del modelo (registros de la tabla alumnos)
+    return render(request, "registros/principal.html", {"alumnos": alumnos})
+# Indicamos el lugar donde se renderizará el resultado de la vista
+# y enviamos la lista de alumnos recuperados
+
+def registrar(request):
+    if request.method == 'POST':
+        form = ComentarioContactoForm(request.POST)
+        if form.is_valid(): #Si los datos que recibe son correctos
+            form.save() #inserta
+            return render(request,'registros/contacto.html')
+        form = ComentarioContactoForm()
+        #Si sale mal se reenvia al formulario los datos ingresados
+        return render(request,'registros/contacto.html', {'form': form})
+    
+def contacto(request):
+    return render(request, "registros/contacto.html")
