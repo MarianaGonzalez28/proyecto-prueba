3f6f1a499e6178d3346dc9518e0866ceca8b0cea
MarianaGonzalez28 <marixd2800@gmail.com>
2026-08-10 23:18:51 -0600

    Actualizacion de proyecto

--- prueba/registros/admin.py
+++ prueba/registros/admin.py
@@ -8,15 +8,29 @@ from .models import ComentarioContacto
 # Register your models here.
 class AdministrarModelo(admin.ModelAdmin):
     readonly_fields = ("created", "updated") #Hace que los campos de fecha y hora no se puedan editar
-    list_display = ('matricula','nombre','carrera','turno')
+    list_display = ('matricula','nombre','carrera','turno','created')
     search_fields = ('matricula','nombre','carrera','turno')
     date_hierarchy = 'created'
     list_filter = ('carrera','turno','created')
 
     list_display_links = ('matricula','nombre') #Para hacer click en la matricula o en el nombre
     list_per_page = 5 #Para poner paginación
-    list_editable = ('turno',) #Editar dentro de la consulta
+    #list_editable = ('turno',) #Editar dentro de la consulta
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name='usuarios').exists():
+            #bloquea los campos
+            return ('matricula','carrera', 'turno')
+            #Cualquier otro usuario que no sea del grupo Usuarios podra editar los campos
+        elif request.user.groups.filter(name='usuario2').exists():
+            #bloquea los campos
+            return ('matricula', 'turno')
+        else:
+            #Bloquea los campos
+            return ('created', 'updated')
+          
 admin.site.register(Alumnos, AdministrarModelo)
+
 
 class AdministrarComentarios(admin.ModelAdmin):
     list_display = ('id','coment')
@@ -24,7 +38,7 @@ class AdministrarComentarios(admin.ModelAdmin):
     date_hierarchy = 'created'
     readonly_fields = ('created', 'id') 
 
-    exclude = ('alumno',) #Para que no se muestre el campo alumno en el formulario de comentarios
+    #exclude = ('alumno',) #Para que no se muestre el campo alumno en el formulario de comentarios
 admin.site.register(Comentario, AdministrarComentarios)
 
 class AdministrarComentariosContacto(admin.ModelAdmin):
--- prueba/registros/views.py
+++ prueba/registros/views.py
@@ -1,7 +1,12 @@
+
+import datetime
 from django.shortcuts import render
-from .models import Alumnos, ComentarioContacto
+from .models import Alumnos, ComentarioContacto, Archivos
 # Accedemos al modelo Alumnos que contine la esctructura de la tabla
-from .forms import ComentarioContactoForm
+from .forms import ComentarioContactoForm, FormArchivos
+from django.shortcuts import get_object_or_404
+from django.contrib import messages
+
 
 # Create your views here.
 def registros(request):
@@ -28,3 +33,132 @@ def contacto(request):
 def consultaContacto(request):
     comentarios = ComentarioContacto.objects.all()
     return render(request, "registros/consultaContacto.html", {"comentarios": comentarios})
+
+def eliminarComentario(request, id, confirmacion ='registros/confirmarEliminacion.html'):
+    comentario = get_object_or_404(ComentarioContacto, id=id)
+    if request.method == 'POST':
+        comentario.delete()
+        comentarios = ComentarioContacto.objects.all()
+        return render(request, 'registros/consultaContacto.html', {'comentarios': comentarios})
+    return render(request, confirmacion, {'object': comentario})
+
+def editarComentario(request, id):
+    comentario = get_object_or_404(ComentarioContacto, id=id)
+    form =  ComentarioContactoForm(request.POST or None, instance=comentario)
+    if form.is_valid():
+        form.save() #Si el regitro ya existe, se modifica
+        comentarios=ComentarioContacto.objects.all()
+        return render(request, "registros/consultaContacto.html", {'comentarios': comentarios})
+    return render(request, 'registros/editarComentario.html', {'comentario': comentario})
+
+def ConsultaComentarioIndividual(request, id):
+    comentario = ComentarioContacto.objects.get(id=id)
+    return render(request, 'registros/editarComentario.html', {'comentario': comentario})
+
+#filter nos retornara los registros que coincidan con los parámetros de busqueda de datos
+def consultar1(request):
+    #con una sola condicion
+    alumnos = Alumnos.objects.filter(carrera="TI")
+    return render(request, "registros/consultas.html", {"alumnos": alumnos})
+
+def consultar2(request):
+    #con una sola condicion
+    alumnos = Alumnos.objects.filter(carrera="TI").filter(turno="Matutino")
+    return render(request, "registros/consultas.html", {"alumnos": alumnos})
+
+def consultar3(request):
+    #si solo deseamos recuperar ciertos datos agregamos la funcion only listando campos que queremos obtenerde
+    #la consulta emplear filter o en el ejemplo all
+    alumnos = Alumnos.objects.all().only("matricula", "nombre", "carrera", "turno", "imagen")
+    return render(request, "registros/consultas.html", {"alumnos": alumnos})
+
+def consultar4 (request):
+    # Recupera alumnos con el nombre "juan"
+    alumnos = Alumnos.objects.filter(nombre__icontains="juan")
+    return render(request, "registros/consultas.html", {"alumnos": alumnos})
+
+def consultar5 (request):
+    # Alumnos registrados en el año 2026
+    alumnos = Alumnos.objects.filter(created__year=2026)
+    return render(request, "registros/consultas.html", {"alumnos": alumnos})
+
+def consultar6 (request):
+    alumnos = Alumnos.objects.filter(nombre__in=["Juan","Ana"])
+    return render(request, "registros/consultas.html", {"alumnos": alumnos})
+
+def consultar7 (request):
+    fechaInicio = datetime.date(2026, 8, 1)
+    fechaFin = datetime.date(2026, 8, 30)
+    alumnos = Alumnos.objects.filter(created__range=(fechaInicio, fechaFin))
+    return render(request, "registros/consultas.html", {"alumnos": alumnos})
+
+def consultar8 (request):
+    #Consultando entre modelos
+    alumnos = Alumnos.objects.filter(comentario__coment__contains='No Inscrito')
+    return render(request, "registros/consultas.html", {"alumnos": alumnos})
+
+def consultasSQL(request):
+    alumnos = Alumnos.objects.raw('SELECT id, matricula, nombre, carrera, turno, created FROM registros_alumnos WHERE carrera="TI" ORDER BY turno DESC')
+    return render(request, "registros/consultas.html", {"alumnos": alumnos})
+
+# Expresiones ORM
+def comentarioContactoPorFecha(request):
+    fecha_inicio = datetime.date(2026, 6, 20)
+    fecha_fin = datetime.date(2026, 8, 4)
+    comentarios = ComentarioContacto.objects.filter(created__range=(fecha_inicio, fecha_fin))
+    return render(request, "registros/consultaContacto.html", {"comentarios": comentarios})
+
+def comentarioContactoExpresion(request):
+    comentarios = ComentarioContacto.objects.filter(mensaje__icontains="Hola")
+    return render(request, "registros/consultaContacto.html", {"comentarios": comentarios})
+
+def comentarioContactoUsuario(request):
+    comentarios = ComentarioContacto.objects.filter(usuario__icontains="Mariana")
+    return render(request, "registros/consultaContacto.html", {"comentarios": comentarios})
+
+# Comienza con una palabra específica, en este ejemplo "No"
+def comentarioContacto1(request):
+    comentarios = ComentarioContacto.objects.filter(mensaje__istartswith="No")
+    return render(request, "registros/consultaContacto.html", {"comentarios": comentarios})
+
+# Terminan con una palabra específica, en este ejemplo "editado"
+def comentarioContacto2(request):
+    comentarios = ComentarioContacto.objects.filter(mensaje__iendswith="editado")
+    return render(request, "registros/consultaContacto.html", {"comentarios": comentarios})
+
+# Consultas SQL
+def comentarioContactoPorFechaSQL(request):
+    comentarios = ComentarioContacto.objects.raw('SELECT id, usuario, mensaje, created FROM registros_comentariocontacto WHERE created BETWEEN "2026-06-20 00:00:00" AND "2026-08-04 23:59:59"')
+    return render(request, "registros/consultaContacto.html", {"comentarios": comentarios})
+
+def comentarioContactoExpresionSQL(request):
+    comentarios = ComentarioContacto.objects.raw('SELECT id, usuario, mensaje, created FROM registros_comentariocontacto WHERE mensaje LIKE "%Hola%"')
+    return render(request, "registros/consultaContacto.html", {"comentarios": comentarios})
+
+def comentarioContactoUsuarioSQL(request):
+    comentarios = ComentarioContacto.objects.raw('SELECT id, usuario, mensaje, created FROM registros_comentariocontacto WHERE usuario="Mariana"')
+    return render(request, "registros/consultaContacto.html", {"comentarios": comentarios})
+
+
+def comentarioContacto1SQL(request):
+    comentarios = ComentarioContacto.objects.raw('SELECT id, usuario, mensaje, created FROM registros_comentariocontacto WHERE mensaje LIKE "No%"')
+    return render(request, "registros/consultaContacto.html", {"comentarios": comentarios})
+
+def comentarioContacto2SQL(request):
+    comentarios = ComentarioContacto.objects.raw('SELECT id, usuario, mensaje, created FROM registros_comentariocontacto WHERE mensaje LIKE "%editado"')
+    return render(request, "registros/consultaContacto.html", {"comentarios": comentarios})
+
+def archivos(request):
+    if request.method == 'POST':
+        form = FormArchivos(request.POST, request.FILES)
+        if form.is_valid():
+            titulo = request.POST['titulo']
+            descripcion = request.POST['descripcion']
+            archivo = request.FILES['archivo']
+            insert = Archivos(titulo=titulo, descripcion=descripcion, archivo=archivo)
+            insert.save()
+            return render(request, "registros/archivos.html")
+        else:
+            messages.error(request, "Error al procesar formulario")
+    else:
+        return render(request, "registros/archivos.html", {'archivo':Archivos})
